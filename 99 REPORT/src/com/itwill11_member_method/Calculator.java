f2c2a90ebd10ee832d29903d2354040a1eaa55d0
mimimi89 <jejek8916@gmail.com>
2020-06-18 09:21:21 +0900

    .

--- 99 REPORT/src/com/itwill11_member_method/Calculator.java
+++ 99 REPORT/src/com/itwill11_member_method/Calculator.java
@@ -2,44 +2,33 @@ package com.itwill11_member_method;
 
 public class Calculator {
 
-	private int a;
-	private int b;
-	
-	/*public int add(int a, int b) {
-		int result=int add;
-		a+b;
-	}
-	
-	
-	/*
-	 * 4개의 멤버메쏘드
+		
+	 /* 4개의 멤버메쏘드
 	 add( 두개의 정수를 인수로 받아서 더한결과를 반환)
-	 substract
+	 subtract
 	 div
 	 multiple
 	 */
 	
-	/*
-	 int method1() {
-		System.out.println("MemberMethodReturn.method1()실행");
-		int result = 2 + 9;
-	
-		 * - 실행흐름을 호출한곳으로반환
-		 * - 실행흐름을 호출한곳으로데이타반환
-		 * - return 할데이타가있는 경우에는 return문 반드시기술하여야 한다 
-		 
-		return result;
-	}
-	boolean isMarried() {
-		System.out.println("MemberMethodReturn.isMarried()실행");
-		return false;
-	}
-	
-	int add(int a,int b) {
-		int r=a+b;
-		return r;
-	}
-	 */
+	 	public int add(int a, int b) {
+			return a+b;
+			
+		}
+		
+		public int sub(int a, int b) {
+			return a-b;
+			
+		}
+		
+		public int mul(int a, int b) {
+			return a*b;
+			
+		}
+		
+		public int div(int a, int b) {
+			return a/b;	
+			
+		}
 	
 	
 }
--- 99 REPORT/src/com/itwill11_member_method/CalculatorMain.java
+++ 99 REPORT/src/com/itwill11_member_method/CalculatorMain.java
@@ -2,19 +2,24 @@ package com.itwill11_member_method;
 
 public class CalculatorMain {
 	public static void main(String[] args) {
-		Calculator calculator=new Calculator();
-		/*
-	int result=calculator.add(23,34);
-	System.out.println(result);
-	result=calculator.sub(23,34);
-	System.out.println(result);
-	result=calculator.mul(23,34);
-	System.out.println(result);
-	result=calculator.div(45435,34);
-	System.out.println(result);
-		 */
 		
+		Calculator calculator=new Calculator();
+		
+		
+		int result=calculator.add(23, 34);
+		System.out.println(result);
+		
+		result=calculator.sub(23, 34);
+		System.out.println(result);
+		
+		result=calculator.mul(23, 34);
+		System.out.println(result);
+		
+		result=calculator.div(45435, 34);
+		System.out.println(result);
+
 	}
+		
 	
 		
 	
