882406fa5612edb0d48f75ea3def957dd2303695
mimimi89 <jejek8916@gmail.com>
2020-06-18 18:50:00 +0900

    .

--- 99 REPORT/src/com/itwill12_member_constructor/StudentConstructorMain.java
+++ 99 REPORT/src/com/itwill12_member_constructor/StudentConstructorMain.java
@@ -9,22 +9,34 @@ public class StudentConstructorMain {
 		Student stu2=new Student(2, "ALEX", 54, 65, 87);
 		Student stu3=new Student(3, "ERIN", 48, 57, 66);
 		
+		
 		stu1.calculateTotal();
 		stu2.calculateTotal();
 		stu3.calculateTotal();
+		
 		
 		stu1.calculateAvg();
 		stu2.calculateAvg();
 		stu3.calculateAvg();
 		
+		
 		stu1.calculateGrade();
 		stu2.calculateGrade();
 		stu3.calculateGrade();
 		
-		stu1.headerPrint();
 		
 		stu1.setName("NICK");
 		
+		
+		int rTot=stu1.getTot();
+		System.out.println(rTot);
+		
+		
+		char rGrade=stu2.getGrade();
+		System.out.println(rGrade);
+		
+		
+		stu1.headerPrint();
 		stu1.print();
 		stu2.print();
 		stu3.print();
