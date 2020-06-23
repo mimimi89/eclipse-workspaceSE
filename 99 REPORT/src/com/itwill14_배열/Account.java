f2e2cb6d2928a52439d48066d13ffa4331066e05
mimimi89 <jejek8916@gmail.com>
2020-06-23 18:33:09 +0900

    .

--- 99 REPORT/src/com/itwill14_배열/Account.java
+++ 99 REPORT/src/com/itwill14_배열/Account.java
@@ -1,23 +1,16 @@
 package com.itwill14_배열;
 
 public class Account {
-	/*
-	 * 속성
-	 * 	- 멤버필드
-	 */
-	private int no;//계좌번호
-	private String owner;//계좌주
-	private int balance;//잔고
-	private double iyul;//이율
-	/*
-	 * 행위(operation)
-	 *  - 멤버메쏘드
-	 */
+	//속성
+	private int no;
+	private String owner;
+	private int balance;
+	private double iyul;
+	
+	//생성자
 	public Account() {
-		// TODO Auto-generated constructor stub
+		
 	}
-	
-	
 	
 	public Account(int no, String owner, int balance, double iyul) {
 		this.no = no;
@@ -26,49 +19,40 @@ public class Account {
 		this.iyul = iyul;
 	}
 
+	//
 
-
-	/*
-	 * 계좌데이타set메쏘드
-	 */
 	public void setAccountData(int no,String owner,int balance,double iyul) {
 		this.no=no;
 		this.owner=owner;
 		this.balance=balance;
 		this.iyul = iyul;
 	}
-	/*
-	 * 입금
-	 */
+	
+	//입금
 	public void deposit(int m) {
 		//this.balance+=m;
 		this.balance=this.balance+m;
 	}
-	/*
-	 * 출금
-	 */
+	
+	//출금
 	public void withdraw(int m) {
 		this.balance=this.balance-m;
 	}
-	/*
-	 * header print
-	 */
+	
+	//header print
 	public static void headerPrint() {
 		System.out.println("-------------------------");
 		System.out.printf("%s %s %s %s%n","번호","계좌주","잔고","이율");
 		System.out.println("-------------------------");
 	}
-	/*
-	 * 출력
-	 */
+
+	//출력
 	public void print() {
 		
 		System.out.printf("%d %6s %6d %4.1f%n",this.no,this.owner,this.balance,this.iyul);
 	}
-	/*
-	 * getter,setter 단축키
-	 *   << alt + shift + s >> --> r
-	 */
+
+	//getter, setter
 	public int getNo() {
 		return no;
 	}
--- 99 REPORT/src/com/itwill14_배열/AccountArrayMain.java
+++ 99 REPORT/src/com/itwill14_배열/AccountArrayMain.java
@@ -5,6 +5,7 @@ public class AccountArrayMain {
 	public static void main(String[] args) {
 		
 		Account acc1=new Account(1111, "MIN", 89000, 5.9);
+		//account 타입의 acc1 객체 생성
 		
 		Account[] accounts= {
 			acc1,	
@@ -17,35 +18,118 @@ public class AccountArrayMain {
 			new Account(8888, "YIM", 21000, 8.7),
 			new Account(9999, "QIM", 71000, 2.7)
 		};
+		//account 배열 타입의 주소값 8개를 가지고 있는 배열 객체 accounts 생성
 		
-		System.out.println("1.은행총계좌수:"+accounts.length);
-		System.out.println("2.은행계좌전체출력");
-		Account.headerPrint();
+		System.out.println();
+		//은행의 총계좌수를 출력
+		System.out.println("1.총계좌수: "+accounts.length+"개");
 		
-		System.out.println("3.은행계좌들 총잔고:");
-		int totBalance=0;
-		
-		System.out.println(totBalance);
-		System.out.println("4.은행계좌들중에서 계좌번호 3333번계좌한개찾아 출력 ");
-		
-		System.out.println("5.은행계좌들중에서 계좌잔고 50000원이상 계좌들찾아 출력 ");
-		System.out.println("6.은행계좌들중에서 계좌이율 5.0이상 계좌들찾아 출력 ");
-		
-		System.out.println("7.6666번계좌 5000원 입금");
-		
-		System.out.println("8.8888번계좌 3000원 출금");
-		
-		
-		System.out.println("9.계좌를 잔고순으로 오름(내림)차순정렬");
-		
-		
-		System.out.println("10.6666계좌의 이율을 9.0로변경");
-		
-		
+		System.out.println();
+		//은행 계좌 전체 출력
+		System.out.println("2.은행 계좌 전체 출력");
 		Account.headerPrint();
 		for (int i = 0; i < accounts.length; i++) {
-			accounts[i].print();	
+			accounts[i].print();
 		}
+		
+		System.out.println();
+		//은행 계좌 총잔고
+		System.out.println("3.은행 계좌 총잔고");
+		int totBalance=0;
+		for (int i = 0; i < accounts.length; i++) {
+			totBalance=totBalance+accounts[i].getBalance();
+			
+		}
+		
+		System.out.println(totBalance+"원");
+		
+		System.out.println();
+		//3333 계좌 출력
+		System.out.println("4.3333 계좌 출력");
+		Account.headerPrint();
+		for (int i = 0; i < accounts.length; i++) {
+			if(accounts[i].getNo()==3333) {
+				accounts[i].print();
+				break;
+				
+			}
+		}
+		
+		System.out.println();
+		//잔고 5000원 이상 계좌 출력
+		System.out.println("5.잔고 5000원 이상 계좌 출력");
+		Account.headerPrint();
+		for (int i = 0; i < accounts.length; i++) {
+			if(accounts[i].getBalance()>=5000) {
+				accounts[i].print();
+			}
+		}
+		
+		
+		System.out.println();
+		//이율 5.0 이상 계좌 출력
+		System.out.println("6.이율 5.0 이상 계좌 출력");
+		Account.headerPrint();
+		for (int i = 0; i < accounts.length; i++) {
+			if(accounts[i].getIyul()>=5.0) {
+				accounts[i].print();
+			}
+		}
+		
+		System.out.println();
+		//6666 계좌에 5000원 입금
+		System.out.println("7.6666계좌에 5000원 입금");
+		for (int i = 0; i < accounts.length; i++) {
+			if(accounts[i].getNo()==6666) {
+				accounts[i].print();
+				accounts[i].deposit(5000);
+				accounts[i].print();
+				break;
+			}
+		}
+		
+		
+		System.out.println();
+		//8888 계좌에서 3000원 입금
+		System.out.println("8.8888계좌에서 3000원 출금");
+		for (int i = 0; i < accounts.length; i++) {
+			if(accounts[i].getNo()==8888) {
+				accounts[i].print();
+				accounts[i].withdraw(3000);
+				accounts[i].print();
+				break;
+			}
+		}
+		
+		System.out.println();
+		//계좌를 잔고순으로 오름차순(큰숫자가 아래로) 정렬
+		System.out.println("9. 잔고순으로 오름차순 정렬");
+		for (int i = 0; i < accounts.length-1; i++) {	//4번만 돌리면 됨
+			for (int j = 0; j < accounts.length-1; j++) {
+				if(accounts[j].getBalance()>accounts[j+1].getBalance()) {
+					Account temAccount=accounts[j];
+					accounts[j]=accounts[j+1];
+					accounts[j+1]=temAccount;
+				}
+				
+			}
+		}
+		Account.headerPrint();
+		for (int i = 0; i < accounts.length; i++) {
+			accounts[i].print();
+		}
+		
+		System.out.println();
+		//6666 계좌 이율을 9.0으로 변경 
+		System.out.println("10. 6666 계좌 이율을 9.0으로 변경");		
+		for (int i = 0; i < accounts.length; i++) {
+			if(accounts[i].getNo()==6666) {
+				accounts[i].print();
+				accounts[i].setIyul(9.0);
+				accounts[i].print();
+			} 
+		}
+		
 		
 	}
 		
