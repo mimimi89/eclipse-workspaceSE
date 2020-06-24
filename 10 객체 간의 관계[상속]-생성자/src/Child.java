e3a9af05529f5fd3c60fbbd66468cec9531d9285
mimimi89 <jejek8916@gmail.com>
2020-06-24 12:54:35 +0900

    .

--- 10 객체 간의 관계[상속]-생성자/src/Child.java
+++ 10 객체 간의 관계[상속]-생성자/src/Child.java
@@ -13,6 +13,20 @@ public class Child extends Parent {
 	} 
 	 */
 	
+	public Child() {
+		//클래스의 생성자 첫번째 라인에서는 어떤 일이 발생?
+		//부모 클래스의 기본(디폴트)생성자가 호출된다
+		super();	//생략가능
+		System.out.println("Child()디폴트생성자");
+		
+		
+		
+		
+		
+	}
+	
+	
+	
 	public int member3;
 	public void method3() {
 		System.out.println("Parent.method3()");
--- 10 객체 간의 관계[상속]-생성자/src/Parent.java
+++ 10 객체 간의 관계[상속]-생성자/src/Parent.java
@@ -3,6 +3,16 @@ public class Parent extends Object{
 	
 	public int member1;
 	public int member2;
+	
+	
+	
+	public Parent() {
+		super();
+		System.out.println("Paren() 기본(디폴트) 생성자");
+	}
+	
+	
+	
 	public void method1() {
 		System.out.println("Parent.method1()");
 	}
