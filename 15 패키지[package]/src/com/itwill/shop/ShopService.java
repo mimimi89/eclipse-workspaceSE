0064ddaf108e9c664e9aafc4c4cf59c820a1487e
mimimi89 <jejek8916@gmail.com>
2020-06-29 14:44:29 +0900

    .

---
+++ 15 패키지[package]/src/com/itwill/shop/ShopService.java
@@ -0,0 +1,5 @@
+package com.itwill.shop;		//패키지의 선언은 클래스 바깥에서 이루어짐
+
+public class ShopService {
+
+}
--- 99999 FUXI/src/com/itwill01_member_method/Calculator.java
+++ 99999 FUXI/src/com/itwill01_member_method/Calculator.java
@@ -1,28 +1,10 @@
 package com.itwill01_member_method;
 /*
- * 4개의 멤버메쏘드를 가짐
+ * 4개의 멤버메쏘드를 가짐(add,sub,mul,div)
  */
 public class Calculator {
 	
-	public int add(int a, int b) {
-		return a+b;
-		
-	}
 	
-	public int sub(int a, int b) {
-		return a-b;
-		
-	}
-	
-	public int div(int a, int b) {
-		return a/b;
-		
-	}
-	
-	public int mul(int a, int b) {
-		return a*b;
-		
-	}
 	
 	
 	
