d43c83415178bc50e0ad9c67da027ca9b3c23293
mimimi89 <jejek8916@gmail.com>
2020-07-03 18:43:04 +0900

    .

--- 08 객체 간의 관계[상속]/src/ObjectChildMain.java
+++ 08 객체 간의 관계[상속]/src/ObjectChildMain.java
@@ -3,10 +3,25 @@ public class ObjectChildMain {
 
 	public static void main(String[] args) {
 		
-		ObjectChild oc = new ObjectChild();	//오브젝트 클래스로 찍어내 객체 oc
-		int hashCode=oc.hashCode();			
+		ObjectChild oc = new ObjectChild();	
+		//ObjectChild 클래스로 찍어내 객체 oc
+		
+		int hashCode=oc.hashCode();
+		/*
+		 * public native int hashCode(); 
+		 * hashCode 메쏘드: 두 객체가 같은 객체인지 확인할 때
+		 */
+
 		System.out.println(hashCode);
+		
+		
 		String str=oc.toString();
+		/* 
+		 * public String toString() {
+		        return getClass().getName() + "@" + Integer.toHexString(hashCode());
+		    }
+		 * toString 메쏘드: 객체가 가지고 있는 정보나 값들을 문자열로 만들어 반환하는 메쏘드
+		 */
 		System.out.println(str);	//객체의 주소지
 		
 		
