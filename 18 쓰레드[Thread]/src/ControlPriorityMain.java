e47ffdda5e71baf15facbb5177775c6bef819c0f
mimimi89 <jejek8916@gmail.com>
2020-07-06 12:26:26 +0900

    .

---
+++ 18 쓰레드[Thread]/src/ControlPriorityMain.java
@@ -0,0 +1,13 @@
+
+public class ControlPriorityMain {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ 18 쓰레드[Thread]/src/PrintUpperThread.java
@@ -0,0 +1,40 @@
+
+public class PrintUpperThread extends Thread{
+	//대문자를 찍는 쓰레드
+	
+	@Override
+	public void run() {
+		while(true) {
+			System.out.print("A");
+			System.out.print("B");
+			System.out.print("C");
+			System.out.print("D");
+			System.out.print("E");
+			System.out.print("F");
+			System.out.print("G");
+			System.out.print("H");
+			System.out.print("I");
+			System.out.print("J");
+			System.out.print("K");
+			System.out.print("L");
+			System.out.print("M");
+			System.out.print("N");
+			System.out.print("O");
+			System.out.print("P");
+			System.out.print("Q");
+			System.out.print("R");
+			System.out.print("S");
+			System.out.print("T");
+			System.out.print("U");
+			System.out.print("V");
+			System.out.print("W");
+			System.out.print("X");
+			System.out.print("Y");
+			System.out.println("Z");
+		}
+	}
+	
+	
+	
+
+}
