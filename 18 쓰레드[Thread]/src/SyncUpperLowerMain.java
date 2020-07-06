80d28abc0b6087db7b622bad0d78ecf64fd236d1
mimimi89 <jejek8916@gmail.com>
2020-07-06 14:38:28 +0900

    .

--- 18 쓰레드[Thread]/src/SyncUpperLowerMain.java
+++ 18 쓰레드[Thread]/src/SyncUpperLowerMain.java
@@ -4,7 +4,9 @@ public class SyncUpperLowerMain {
 	public static void main(String[] args) {
 		Object monitor=new Object();
 		SyncUpperThread ut=new SyncUpperThread(monitor);
+		ut.setPriority(8);
 		SyncLowerThread lt=new SyncLowerThread(monitor);
+		lt.setPriority(2);
 		ut.start();
 		lt.start();
 		
