fbb1a87a9489901fe6cbdede29301fbb8ff10ff0
mimimi89 <jejek8916@gmail.com>
2020-07-06 11:57:27 +0900

    .

--- 18 쓰레드[Thread]/src/ControlStopThread.java
+++ 18 쓰레드[Thread]/src/ControlStopThread.java
@@ -14,23 +14,47 @@ public class ControlStopThread extends Thread{
 
 	@Override
 	public void run() {
-		while(isPlay) {
-			
-			try {
-				System.out.println("ControlStopThread: "+count);
-				Thread.sleep(500);
+		while (isPlay) {
 				count++;
-				
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+				if(count==10) {
+					isPlay=false;
+				}
+				System.out.print("A");
+				System.out.print("B");
+				System.out.print("C");
+				System.out.print("D");
+				System.out.print("E");
+				System.out.print("F");
+				System.out.print("G");
+				System.out.print("H");
+				System.out.print("I");
+				System.out.print("J");
+				System.out.print("K");
+				System.out.print("L");
+				System.out.print("M");
+				System.out.print("N");
+				System.out.print("O");
+				System.out.print("P");
+				System.out.print("Q");
+				System.out.print("R");
+				System.out.print("S");
+				System.out.print("T");
+				System.out.print("U");
+				System.out.print("V");
+				System.out.print("W");
+				System.out.print("X");
+				System.out.print("Y");
+				System.out.println("Z");
 		}
-		
-		
-		
-		
-		
-		
+		return;
+	}
+
+	public boolean isPlay() {
+		return isPlay;
+	}
+
+	public void setPlay(boolean isPlay) {
+		this.isPlay = isPlay;
 	}
 
 }
--- 18 쓰레드[Thread]/src/ControlStopThreadMain.java
+++ 18 쓰레드[Thread]/src/ControlStopThreadMain.java
@@ -1,13 +1,29 @@
 
 public class ControlStopThreadMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		System.out.println("1.main");
 		ControlStopThread cst=new ControlStopThread();
 		cst.setName("stop thread");
-				
-
+		cst.start();
+		System.out.println("2.main sleep");
+		Thread.sleep(4556); 
+		
+		//case1: Thread.stop() 사용하지 않는 게 좋음
+		//cst.stop();
+		
+		//case2: isPlay 변수를 false로 바꿔주면 됨
+		//cst.setPlay(false);
+		
+		//case3: 쓰레드가 할 일을 다 하면 스스로 종료
+		
+		
+		//join: main 쓰레드는 cst 쓰레드가 죽을 때까지 대기한다.
+		cst.join();
+		System.out.println("99.main return");
+		
+	
 	}
 
 }
