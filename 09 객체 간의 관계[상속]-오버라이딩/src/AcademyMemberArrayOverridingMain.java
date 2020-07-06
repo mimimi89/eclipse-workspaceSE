6a3fcc4506dea4242b1e6d928df1f52c88dd2ac8
mimimi89 <jejek8916@gmail.com>
2020-07-06 17:55:58 +0900

    .

--- 09 객체 간의 관계[상속]-오버라이딩/src/AcademyMemberArrayOverridingMain.java
+++ 09 객체 간의 관계[상속]-오버라이딩/src/AcademyMemberArrayOverridingMain.java
@@ -1,10 +1,10 @@
+///
 
 public class AcademyMemberArrayOverridingMain {
 
 	public static void main(String[] args) {
 		
 		AcademyStudent[] students=new AcademyStudent[3];
-		//academystudent 3명을 담을 수 있는 배열 객체 생성
 		AcademyGangsa[] Gangsas=new AcademyGangsa[2];
 		AcademyStaff[] Staffs=new AcademyStaff[2];
 		
@@ -19,7 +19,7 @@ public class AcademyMemberArrayOverridingMain {
 		Staffs[0]=new AcademyStaff(5, "lim", "총무부");
 		Staffs[1]=new AcademyStaff(6, "him", "홍보부");
 		
-		
+			
 		System.out.println("----------AcademyMember 전체출력-------------");
 		System.out.println();
 		
