8f36d54c1165631c51b0147d8b95fdbd9c1c5e61
mimimi89 <jejek8916@gmail.com>
2020-07-06 10:15:23 +0900

    .

--- 08 객체 간의 관계[상속]/src/Child2.java
+++ 08 객체 간의 관계[상속]/src/Child2.java
@@ -3,6 +3,22 @@
 public class Child2 extends Parent {	
 //부모 클래스를 상속받는 자식2 클래스
 	
+	/*
+	public int member1;
+	public int member2;
+	
+	public void method1() {
+		System.out.println("Parent.method1()");
+	}
+	public void method2() {
+		System.out.println("Parent.method2()");
+	}
+	
+	public void parentPrint() {
+		System.out.print(member1+"\t"+member2+"\t");
+	}
+	*/
+	
 	public int member4;		//상속 받은 2개 포함, 3개
 	public void method4() {
 		System.out.println("Child2.method4()");
