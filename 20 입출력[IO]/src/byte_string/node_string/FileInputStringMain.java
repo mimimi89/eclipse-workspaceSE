b43b556702b7919db5b122f8acd75c1e774b8064
mimimi89 <jejek8916@gmail.com>
2020-07-07 11:48:04 +0900

    .

--- 20 입출력[IO]/src/byte_string/node_string/FileInputStringMain.java
+++ 20 입출력[IO]/src/byte_string/node_string/FileInputStringMain.java
@@ -1,8 +1,30 @@
 package byte_string.node_string;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/*
+ * 1. 데이타의 source 선정(파일)
+ * 2. 입력스트림(InputStream) 객체 생성(FileInputStream)
+ * 3. 입력스트림(InputStream)에 1바이트씩 읽는다.(read)   
+ * 4. 입력스트림(InputStream)을 닫는다.(close)
+ */
 public class FileInputStringMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		FileInputStream fis=new FileInputStream("fileOut.dat");
+		
+		int readByte=fis.read();
+		System.out.println("1.byte: "+Integer.toBinaryString(readByte));
+		//System.out.println("1.byte: "+readByte);
+		
+		readByte=fis.read();
+		System.out.println("2.byte: "+Integer.toBinaryString(readByte));
+		
+		
+		fis.close();
+		
 		
 		
 		
--- 20 입출력[IO]/src/byte_string/node_string/FileOutputStringMain.java
+++ 20 입출력[IO]/src/byte_string/node_string/FileOutputStringMain.java
@@ -13,11 +13,38 @@ import java.io.IOException;
 public class FileOutputStringMain {
 
 	public static void main(String[] args) throws IOException {
-		FileOutputStream fos=new FileOutputStream("fileOut.txt");
-		fos.write(65);
-		fos.write(66);
-		fos.close();
+		FileOutputStream fos=new FileOutputStream("fileOut.dat");
 		
+		/*
+		 * <<OutputStream>>
+		 * public abstract void write(int b) throws IOException
+		  	- Writes the specified byte to this output stream. 
+		  	- The general contract for write is 
+		  	that one byte is written to the output stream. 
+		  	- The byte to be written is the eight low-order bits of the argument b. 
+		 */
+		
+		fos.write(65);
+		// 00000000|00000000|00000000|01000001
+		fos.write(66);
+		// 00000000|00000000|00000000|01000010
+		
+		fos.write(2147483647);
+		// 01111111|11111111|11111111|11111111
+		fos.write(255);
+		// 00000000|00000000|00000000|11111111
+		//앞의 3바이트는 날라감
+		
+
+		fos.write('C');
+		fos.write('D');
+		for (int i = 0; i < 256; i++) {
+			fos.write(i);
+		}
+		
+		
+		fos.close();
+		System.out.println("FileOutputStream.write!!!");
 		
 		
 		
