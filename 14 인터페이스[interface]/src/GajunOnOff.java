f871a6fafcbc3da06c83ee2c4361b090448ffe95
mimimi89 <jejek8916@gmail.com>
2020-07-09 10:09:52 +0900

    .

--- 14 인터페이스[interface]/src/GajunOnOff.java
+++ 14 인터페이스[interface]/src/GajunOnOff.java
@@ -1,5 +1,6 @@
+///
 public interface GajunOnOff {
-
+//켜고 끄는 기능을 가진 두 개의 메쏘드 선언한 GajunOnOff Interface
     public void turnOn();
     public void turnOff();
 
--- 14 인터페이스[interface]/src/GajunVolume.java
+++ 14 인터페이스[interface]/src/GajunVolume.java
@@ -1,5 +1,7 @@
+///
 
 public interface GajunVolume {
+//소리를 높이고 줄이는 기능을 가진 두 개의 메쏘드를 선언한 GajunVolume Interface
 	public void up();
 	public void down();
 
