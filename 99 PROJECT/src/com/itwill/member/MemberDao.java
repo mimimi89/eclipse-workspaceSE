9a738e7e2c7b6806bbdcd833fdb921f430a18edc
mimimi89 <jejek89@gmail.com>
2020-07-18 22:45:27 +0800

    .

--- 99 PROJECT/src/com/itwill/member/MemberDao.java
+++ 99 PROJECT/src/com/itwill/member/MemberDao.java
@@ -10,18 +10,36 @@ import java.util.ArrayList;
 import com.itwill.account.Account;
 
 public class MemberDao {
+	/*
+	 * 멤버필드
+	 */
 	private File membersFile;
+	//file 자료형으로 하는  membersfile 
+	
+	/*
+	 * 생성자 메쏘드
+	 */
 	public MemberDao() throws Exception{
 		membersFile=new File("members.ser");
 		if(!membersFile.exists()) {
+			//membersFile이 존재하는지 여부 확인
 			System.out.println("파일생성[members.ser]");
+			//ObjectOutputStream 인스턴스르 파일에 저장하는 과정
 			ObjectOutputStream oos
 				=new ObjectOutputStream(new FileOutputStream(membersFile));
 			oos.writeObject(new ArrayList<Account>());
 		}
 	}
+	
 	/**********************************************/
+	
+	/*
+	 * input, output
+	 */
 	private ArrayList<Member> readFile() throws Exception{
+		//ArrayList<Member>를 반환하는 readFile 메쏘드
+		
+		//ObjectInputStream 피일에서 인스턴스를 복원하는 과정
 		ObjectInputStream ois=
 				new ObjectInputStream(
 						new FileInputStream(membersFile));
@@ -29,22 +47,30 @@ public class MemberDao {
 		ois.close();
 		return memberList;
 	}
+	
 	private void writeFile(ArrayList<Member> memberList) throws Exception{
+		//파일에서 불러온 memberList를 쓰는 writeFile 메쏘드
 		ObjectOutputStream oos=
 				new ObjectOutputStream(
 						new FileOutputStream(membersFile));
 		oos.writeObject(memberList);
 		oos.close();
 	}
+	
 	/**********************************************/
+	
 	/*
-	 * create
+	 * 1. CRUD-create
 	 */
 	public boolean create(Member addMember) throws Exception{
 		boolean isSuccess=false;
+		//성공여부 false로 초기화
 		ArrayList<Member> memberList=this.readFile();
+		//읽어낸 파일을 멤버리스트에 담아놓기
 		if(isDuplicateId(addMember.getId())) {
+			//추가한 멤버의 아이디를 인자로 넣어 중복여부 체크
 			isSuccess=false;
+			//false를 
 			return isSuccess;
 		}
 		isSuccess=true;
